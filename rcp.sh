c136f4ad73d20aa4fdc065d87ba1fd04ab6a4457
9191521 <kexibushini111@gmail.com>
2023-05-07 20:09:57 +0800

    Update rcp.sh

--- rcp.sh
+++ rcp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 # qBittorrent 'Run after completion':
-# /home/ccf2012/torcp/rcp.sh "%F"  "%N"
+# /home/ytyt/torcp/rcp.sh "%F"  "%N"
 
 # # example 1: hardlink to local emby folder
-torcp "$1" -d "/home/ccf2012/emby/" -s --tmdb-api-key your_tmdb_api_key --lang cn,jp  >>/home/ccf2012/rcp.log 2>>/home/ccf2012/rcp_error.log
+torcp "$1" -d "/home/ytyt/emby/" -s --tmdb-api-key bbe6246a002bc537400655a419351302 --lang cn,jp  >>/home/ytyt/rcp.log 2>>/home/ytyt/rcp_error.log
 
 # # example 2: rclone copy to gd drive
 # torcp "$1" -d "/home/ccf2012/emby/$2/" -s --tmdb-api-key <tmdb api key> --lang cn,jp
